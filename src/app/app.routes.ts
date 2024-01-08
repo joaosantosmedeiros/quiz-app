c8aae1c04a864f857b81c258253513e631713f36
joaosantosmedeiros <jopesame@gmail.com>
2024-01-08 14:28:01 -0300

    feat: create home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
+import { ResultsComponent } from './pages/results/results.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {
+    path: '',
+    component: HomeComponent,
+    children: [{ path: 'hp', redirectTo: 'results' }, { path: 'st', redirectTo: 'results' }, { path: 'pj', redirectTo: 'results' }],
+  },
+  { path: 'results', component: ResultsComponent },
+
+
+
 ];
