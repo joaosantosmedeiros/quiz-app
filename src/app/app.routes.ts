2fe21912bb47a08b850d9836503e28a7dc75e5d0
joaosantosmedeiros <jopesame@gmail.com>
2024-01-24 09:27:24 -0300

    feat: add star wars page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,12 @@
 import { Routes } from '@angular/router';
 import { ResultsComponent } from './pages/results/results.component';
 import { HomeComponent } from './pages/home/home.component';
+import { StarWarsComponent } from './pages/star-wars/star-wars.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    children: [{ path: 'hp', redirectTo: 'results' }, { path: 'st', redirectTo: 'results' }, { path: 'pj', redirectTo: 'results' }],
-  },
+  { path: '', component: HomeComponent },
+  { path: 'hp', redirectTo: 'results' },
+  { path: 'st', component: StarWarsComponent },
+  { path: 'pj', redirectTo: 'results' },
   { path: 'results', component: ResultsComponent },
-
-
-
 ];
