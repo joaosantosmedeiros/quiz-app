e5a7038a73ae1c2ca613fba89a67657ecded2678
joaosantosmedeiros <jopesame@gmail.com>
2024-01-29 08:47:00 -0300

    feat: add harry potter page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { ResultsComponent } from './pages/results/results.component';
 import { HomeComponent } from './pages/home/home.component';
 import { StarWarsComponent } from './pages/star-wars/star-wars.component';
+import { HarryPotterComponent } from './pages/harry-potter/harry-potter.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'hp', redirectTo: 'results' },
+  { path: 'hp', component: HarryPotterComponent },
   { path: 'st', component: StarWarsComponent },
   { path: 'pj', redirectTo: 'results' },
   { path: 'results', component: ResultsComponent },
---
+++ src/app/pages/harry-potter/harry-potter.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import {universes} from '../../../assets/data/questions.json'
+import { CommonModule } from '@angular/common';
+
+type Question = {
+  id: number;
+  img: string;
+  title: string;
+  options: {
+    letter: string;
+    option: string;
+    alias: string;
+  }[];
+};
+
+@Component({
+  selector: 'app-harry-potter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './harry-potter.component.html',
+  styleUrl: './harry-potter.component.css',
+})
+export class HarryPotterComponent {
+  questions: Question[] = universes.harryPotter.questions;
+  index: number = 0;
+  selectedQuestion: Question = this.questions[this.index];
+  answers: string[] = [];
+  isFinished: boolean = false;
+  result: string = '';
+
+  selectAnswer(alias: string) {
+    this.index++;
+    this.answers.push(alias);
+    if (this.index < 9) {
+      this.selectedQuestion = this.questions[this.index];
+    } else {
+      this.isFinished = true;
+      this.result = this.findResult(this.answers);
+    }
+  }
+
+  findResult(answers: string[]): string {
+    const a = answers.filter((answ) => answ == 'A').length;
+    let response = 'mal';
+    if (a > 4) {
+      response = 'bem';
+    }
+
+    this.addCookie(response);
+
+    console.log(document.cookie);
+    return response;
+  }
+
+  addCookie(response: string) {
+    if (!document.cookie.includes('results=')) {
+      document.cookie = `results=${response}`;
+    } else {
+      let results = document.cookie.split('results=')[1];
+      results += `,${response}`;
+      document.cookie = `results=${results}`;
+    }
+  }
+}
